ba6d9de63561c8b2ee207675c139a88d08bdbfbc
pabcrudel <pabcrudel@gmail.com>
2023-06-21 18:00:00 +0200

    Client set connection in constructor

---
+++ src/connection/MyClient.java
@@ -0,0 +1,42 @@
+package connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MyClient {
+//	Attributes
+	private Socket socket;
+	private BufferedReader input;
+	private PrintStream output;
+	private String ip;
+	private int port;
+	
+//	Constructor
+	public MyClient(String ip, int port) {
+		try {
+			this.socket = new Socket(ip, port);
+		} 
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+//	Main
+	public static void main(String[] args) {
+		Scanner k = new Scanner(System.in);
+
+		System.out.println("which ip and port do you want to connect?");
+		System.out.print("ip: ");
+		String ip = k.nextLine();
+		System.out.print("port: ");
+		int port = k.nextInt(); k.nextLine();
+		
+		MyClient client = new MyClient(ip, port);
+	}
+}
