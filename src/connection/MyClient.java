5298d4b5226309bd9c139cd36edd28ac60a127ef
pabcrudel <pabcrudel@gmail.com>
2023-06-21 18:01:39 +0200

    Input and output created in constructor

--- src/connection/MyClient.java
+++ src/connection/MyClient.java
@@ -2,6 +2,7 @@ package connection;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -19,6 +20,8 @@ public class MyClient {
 	public MyClient(String ip, int port) {
 		try {
 			this.socket = new Socket(ip, port);
+			this.input = new BufferedReader( new InputStreamReader(this.socket.getInputStream()));
+			this.output = new PrintStream(this.socket.getOutputStream());
 		} 
 		catch (UnknownHostException e) {
 			e.printStackTrace();
